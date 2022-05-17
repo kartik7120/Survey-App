8a9d7a10a7df92a1124ff1778dac2211e968f0ff
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-17 16:20:03 +0530

    Added styling for options buttons

--- src/components/Option.jsx
+++ src/components/Option.jsx
@@ -1,14 +1,33 @@
 import { TextField } from "@mui/material";
+import { IconButton } from "@mui/material";
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
+
+// import {} from "@mui/icons-material";
 
 function Option(props) {
   return (
-    <TextField
-      helperText=""
-      id="demo-helper-text-aligned-no-helper"
-      label={`Option ${props.optionNo}`}
-      sx={{ marginBottom: "0.5em", width: "30em", alignSelf: "flex-start" }}
-      className={"option-text-feild"}
-    />
+    <>
+      <div className="option-flex-box">
+        <TextField
+          helperText=""
+          id="demo-helper-text-aligned-no-helper"
+          label={`Option ${props.optionNo}`}
+          sx={{ marginBottom: "0.5em", width: "30em", alignSelf: "flex-start" }}
+          className={"option-text-feild"}
+        ></TextField>
+        {props.optionNo > 2 ? (
+          <IconButton
+            aria-label="delete"
+            size="large"
+            sx={{ display: "inline" }}
+          >
+            <DeleteOutlinedIcon sx={{ display: "inline" }} />
+          </IconButton>
+        ) : (
+          ""
+        )}
+      </div>
+    </>
   );
 }
 export default Option;
