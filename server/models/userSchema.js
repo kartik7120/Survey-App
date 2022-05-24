0f6332e6b38b04bcd76ba7c7f270af16bd851f8a
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-24 10:14:15 +0530

    Made the user Schema for the backend

---
+++ server/models/userSchema.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Poll = require("./pollSchema");
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        require: true
+    },
+    polls: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Poll"
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
