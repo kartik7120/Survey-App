a566459617877a084600fed3d9be2f8caaa12782
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-27 10:42:20 +0530

    Added poll feild for each user when a poll is created in backend

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -1,6 +1,8 @@
 const Poll = require("../models/pollSchema");
 const express = require("express");
 const router = express.Router();
+const User = require("../models/userSchema");
+const { default: mongoose } = require("mongoose");
 
 router.get("/allPolls", async (req, res, next) => {
     try {
@@ -15,28 +17,36 @@ router.get("/allPolls", async (req, res, next) => {
 
 router.post("/create", async (req, res, next) => {
     try {
-        const { title, description, options } = req.body;
-        console.log("Title = ", title);
-        console.log("description = ", description);
-        console.log("options = ", options);
-        let op = [];
-        for (let [key, value] of Object.entries(options)) {
-            op.push(value); // values for options
+        if (req.user) {
+            const { title, description, options } = req.body;
+            console.log("Title = ", title);
+            console.log("description = ", description);
+            console.log("options = ", options);
+            let op = [];
+            for (let [, value] of Object.entries(options)) {
+                op.push(value); // values for options
+            }
+            let vote = [];
+            for (let i = 0; i < op.length; i++) {
+                vote.push(0); // initial values for votes for each option value
+            }
+            const newPoll = new Poll({
+                _id: new mongoose.Types.ObjectId(),
+                title: title,
+                description: description,
+                options: op,
+                votes: vote
+            });
+            await newPoll.save()
+                .then(() => console.log("Successfully saved to the database"))
+                .catch((err) => console.log("Error in saving to the database", err));
+
+            const user = await User.findById({ _id: req.user.id });
+            user.polls = newPoll._id;
+            res.json("Hello I am the POST poll route");
         }
-        let vote = [];
-        for (let i = 0; i < op.length; i++) {
-            vote.push(0); // initial values for votes for each option value
-        }
-        const newPoll = new Poll({
-            title: title,
-            description: description,
-            options: op,
-            votes: vote
-        });
-        await newPoll.save()
-            .then(() => console.log("Successfully saved to the database"))
-            .catch((err) => console.log("Error in saving to the database", err));
-        res.json("Hello I am the POST poll route");
+        else
+            res.json("User is not logged in so cannot create a poll");
     } catch (error) {
         next(error);
     }
