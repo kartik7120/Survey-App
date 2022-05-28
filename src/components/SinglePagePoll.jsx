5fd59bab85a7b5c11d84a4540bc01f1e7dec54ee
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-28 16:05:30 +0530

    Added useParams hook in SinglePagePoll component

--- src/components/SinglePagePoll.jsx
+++ src/components/SinglePagePoll.jsx
@@ -1,4 +1,8 @@
+import { useParams } from "react-router-dom";
+
 function SinglePagePoll(props) {
+  const { id } = useParams();
+  console.log(id);
   return <h1>I am a single page poll </h1>;
 }
 export default SinglePagePoll;
