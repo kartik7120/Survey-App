bf2ba8efad2ebe5446222c26b229b7251c6d1ad6
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-29 11:36:35 +0530

    made the profile component stateful

--- src/routes/Profile.jsx
+++ src/routes/Profile.jsx
@@ -5,17 +5,36 @@ function Profile(props) {
   const signInObject = React.useContext(signInContext);
   const signInState = signInObject.signInState;
   console.log("signInState = ", signInState);
-  React.useEffect(function () {
-    fetch(`/users/${signInState._id}`)
-      .then((jsonData) => jsonData.json())
-      .then((data) => console.log("Data from the /users/:id route", data))
-      .catch((err) =>
-        console.log(
-          "Some error occured while fetching data from /users/:id route",
-          err
-        )
-      );
-  });
-  return <h1>I am the profile route</h1>;
+  const [userDataState, setUserDataState] = React.useState(null);
+  React.useEffect(
+    function () {
+      fetch(`/users/${signInState._id}`)
+        .then((jsonData) => jsonData.json())
+        .then((data) => {
+          console.log("Data from the /users/:id route", data);
+          const body = JSON.parse(data);
+          setUserDataState(function () {
+            return {
+              _id: body._id,
+              polls: body.polls,
+              username: body.username,
+            };
+          });
+        })
+        .catch((err) =>
+          console.log(
+            "Some error occured while fetching data from /users/:id route",
+            err
+          )
+        );
+    },
+    [signInState._id]
+  );
+
+  return (
+    <>
+      <h1>I am the profile route</h1>
+    </>
+  );
 }
 export default Profile;
