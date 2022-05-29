4c5f7e9d7efe43a8018a1778276f669bbf3e9b97
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-29 09:41:52 +0530

    Refactoring SinglePoll component

--- src/components/SinglePoll.jsx
+++ src/components/SinglePoll.jsx
@@ -1,13 +1,8 @@
-// import { Button } from "@mui/material";
 import React from "react";
-// import PollChoice from "../components/pollChoice";
-// import { signInContext } from "./Navbar";
 import { useNavigate } from "react-router";
 
 function SinglePoll(props) {
   let navigate = useNavigate();
-  // const signInObject = React.useContext(signInContext);
-  // const signInState = signInObject.signInState;
   const [state] = React.useState({
     id: props.id,
     title: props.title,
@@ -26,57 +21,11 @@ function SinglePoll(props) {
     navigate(`../poll/${state.id}`);
   }
 
-  // function handleSubmit(e) {
-  //   if (signInState !== false) {
-  //     e.preventDefault();
-  //     const fetchConfig = {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Accept: "*/*",
-  //       },
-  //       body: JSON.stringify(state),
-  //     };
-  //     fetch(`/poll/updateVotes/${state.id}`, fetchConfig)
-  //       .then((jsonData) => jsonData.json())
-  //       .then((data) => {
-  //         console.log("Data recieved from patch request", data);
-  //         const objData = JSON.parse(data);
-  //         console.log(objData);
-  //         setState(function (oldState) {
-  //           return {
-  //             ...oldState,
-  //             id: objData._id,
-  //             title: objData.title,
-  //             options: objData.options,
-  //             votes: objData.votes,
-  //             description: objData.description,
-  //           };
-  //         });
-  //       })
-  //       .catch((err) =>
-  //         console.log("Error occured while making a PATCH request", err)
-  //       );
-  //   } else {
-  //     navigate("/Signup");
-  //   }
-  // }
-
   return (
     <div className="poll" onClick={handleClick}>
-      {/* <form action="" method="post" onSubmit={handleSubmit}> */}
       <h1>{state.title}</h1>
       <p>{state.description}</p>
       <p>{totalVotes} votes</p>
-      {/* <PollChoice
-          options={state.options}
-          votes={state.votes}
-          setState={setState}
-        /> */}
-      {/* <Button variant="contained" color="warning" type="submit">
-          Vote
-        </Button> */}
-      {/* </form> */}
     </div>
   );
 }
