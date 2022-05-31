20cc6237e887806fce6500b3eba15b81be043c3d
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-05-31 18:55:44 +0530

    Adding a tween transition whileTapping polls in allPolls tab

--- src/components/SinglePoll.jsx
+++ src/components/SinglePoll.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { Card, CardActionArea, CardContent } from "@mui/material";
-import "../allPolls.css";
+import { motion } from "framer-motion";
+
 function SinglePoll(props) {
+  const variants = {
+    whiletap: {
+      scale: 0.9,
+      duration: 0.2,
+      transition: "tween",
+    },
+  };
   let navigate = useNavigate();
   const [state] = React.useState({
     id: props.id,
@@ -22,17 +30,26 @@ function SinglePoll(props) {
   }
 
   return (
-    <Card onClick={handleClick}>
-      <CardContent>
-        <CardActionArea>
-          <div className="poll">
-            <h1>{state.title}</h1>
-            <p>{state.description}</p>
-            <p>{totalVotes} votes</p>
-          </div>
-        </CardActionArea>
-      </CardContent>
-    </Card>
+    <motion.div
+      variants={variants}
+      animate={{ y: 50 }}
+      transition={{ duration: 1 }}
+      whileTap="whiletap"
+    >
+      <Card sx={{ margin: "1em" }}>
+        <CardContent>
+          <CardActionArea onClick={handleClick}>
+            <div className="poll">
+              <h1>{state.title}</h1>
+              <p>{state.description}</p>
+              <p>{totalVotes} votes</p>
+            </div>
+          </CardActionArea>
+        </CardContent>
+        {/* <div className="shadow-1"></div>
+      <div className="shadow-2"></div> */}
+      </Card>
+    </motion.div>
   );
 }
 export default SinglePoll;
