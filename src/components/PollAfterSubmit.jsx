ff74d4eca51fc91817026655c1d13545ee00d827
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-01 08:42:09 +0530

    Added hover animation using frame motion on poll after submit

--- src/components/PollAfterSubmit.jsx
+++ src/components/PollAfterSubmit.jsx
@@ -1,11 +1,30 @@
+import { motion } from "framer-motion";
 function PollAfterSubmit(props) {
+  const variants = {
+    hover: {
+      scale: 1.1,
+    },
+  };
   return (
     <>
       {props.options.map((option, index) => {
         return (
-          <div>
-            {option} <span>{props.votes[index]}</span>
-          </div>
+          <motion.div
+            animate={{ scale: 1 }}
+            variants={variants}
+            transition={{ duration: 0.5 }}
+            whileHover="hover"
+          >
+            <div
+              style={{
+                backgroundColor: "#4b5256",
+                margin: "1em",
+                color: "whitesmoke",
+              }}
+            >
+              {option} <span>{props.votes[index]}</span>
+            </div>
+          </motion.div>
         );
       })}
     </>
