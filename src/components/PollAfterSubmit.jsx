eea0cc9c4ede6aa3e1774936977807f4e8dc37c4
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-02 15:33:57 +0530

    Fixing the poll fill bar

--- src/components/PollAfterSubmit.jsx
+++ src/components/PollAfterSubmit.jsx
@@ -1,4 +1,3 @@
-import { Container } from "@mui/material";
 import { motion } from "framer-motion";
 function PollAfterSubmit(props) {
   const variants = {
@@ -13,37 +12,37 @@ function PollAfterSubmit(props) {
   );
   return (
     <>
-      <Container maxWidth="xl">
-        {props.options.map((option, index) => {
-          return (
-            <motion.div
-              animate={{ scale: 1 }}
-              variants={variants}
-              transition={{ duration: 0.4 }}
-              whileHover="hover"
-              layout
-              key={index * 521}
-            >
-              <div className="poll-after-submit-option">
-                <div className="indivisual-poll">
-                  <div>{option}</div> <div>{props.votes[index]}</div>
-                </div>
-                <motion.div
-                  className="poll-fill-in"
-                  animate={{
-                    width: `${props.votes[index]}%`,
-                  }}
-                  layout
-                  transition={{ delay: 0.5, type: "tween" }}
-                ></motion.div>
+      {/* <Container> */}
+      {props.options.map((option, index) => {
+        return (
+          <motion.div
+            animate={{ scale: 1 }}
+            variants={variants}
+            transition={{ duration: 0.4 }}
+            whileHover="hover"
+            layout
+            key={index * 521}
+          >
+            <div className="poll-after-submit-option">
+              <div className="indivisual-poll">
+                <div>{option}</div> <div>{props.votes[index]}</div>
               </div>
-            </motion.div>
-          );
-        })}
-        <span className="totalVotes">
-          <span className="voteNumber">{totalVotes}</span> Votes
-        </span>
-      </Container>
+              <motion.div
+                className="poll-fill-in"
+                animate={{
+                  width: `${props.votes[index]}%`,
+                }}
+                layout
+                transition={{ delay: 0.5, type: "tween" }}
+              ></motion.div>
+            </div>
+          </motion.div>
+        );
+      })}
+      <span className="totalVotes">
+        <span className="voteNumber">{totalVotes}</span> Votes
+      </span>
+      {/* </Container> */}
     </>
   );
 }
