81bcaf795cd76a0baeb57970dfacf4d5326c1465
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-03 16:17:32 +0530

    Fixed bug of proxy not loading images and add styles to the upper part of the home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,22 +1,37 @@
-import { Button, Container } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 import React from "react";
 import "../homeStyle.css";
 
 function Home(props) {
-  function handleClick(e) {}
   return (
-    <Container maxWidth="xl" className="homeBody" sx={{ display: "flex" }}>
-      <div className="flex-box-home">
-        <h1>Voting app</h1>
-        <p>Made using React</p>
+    <Container maxWidth="xl">
+      <div className="upperFlexBox">
+        <div className="upperText">
+          <Typography
+            variant="h3"
+            component="h3"
+            gutterBottom
+            sx={{ fontSize: "4em" }}
+          >
+            Create Amazing Polls in Seconds
+          </Typography>
+          <Typography
+            variant="body1"
+            component="p"
+            sx={{
+              fontWeight: "0.5em",
+              fontSize: "2em",
+            }}
+            gutterBottom
+          >
+            Create a poll, Share with friends or Embed on your site. Unlimited
+            Polls - Unlimited Votes - Limitless Customisation - ABSOLUTELY FREE!
+          </Typography>
+        </div>
+        <div className="homeBody">
+          <img src="/images/vote4.jpg" className="homeImg" alt="" />
+        </div>
       </div>
-      <Button
-        variant="contained"
-        onClick={handleClick}
-        className="getStartedButton"
-      >
-        Get Started
-      </Button>
     </Container>
   );
 }
