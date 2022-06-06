0980650d8d6f1ca80d6bdda575faa6a9496422ff
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-06 19:18:20 +0530

    Added some changes to the /updateVotes/:id route to add ids of the user that voted on the page

--- server/routes/poll.js
+++ server/routes/poll.js
@@ -95,26 +95,39 @@ router.post("/create", async (req, res, next) => {
     }
 });
 
-router.patch("/updateVotes/:id", async (req, res) => {
-    const { _id, targetValue } = req.body;
+router.patch("/updateVotes/:id", async (req, res, next) => {
+    try {
+        const { _id, targetValue } = req.body;
+        console.log("req.user in updateVotes = ", req.user);
 
-    const poll = await Poll.findById({ _id });
-    let updateIdx = -1;
-    const optionArray = poll.options;
-    for (let i = 0; i < optionArray.length; i++) {
-        if (optionArray[i] === targetValue) {
-            updateIdx = i;
-            break;
+        const poll = await Poll.findById({ _id });
+        let updateIdx = -1;
+        const optionArray = poll.options;
+        const userVotedArray = poll.userVoted;
+
+        for (let i = 0; i < optionArray.length; i++) {
+            if (optionArray[i] === targetValue) {
+                updateIdx = i;
+                break;
+            }
         }
+
+        if (req.user) {
+            console.log("Inside this if condition if user has logged in");
+            userVotedArray.push(req.user._id);
+        }
+
+        const votesArray = poll.votes;
+        votesArray[updateIdx] = votesArray[updateIdx] + 1;
+
+        const newPoll = await Poll.findByIdAndUpdate({ _id }, { $set: { "votes": votesArray, "userVoted": userVotedArray } }, { new: true });
+        res.contentType("application/json");
+
+        res.json(JSON.stringify(newPoll));
+    } catch (error) {
+        next(error);
     }
 
-    const votesArray = poll.votes;
-    votesArray[updateIdx] = votesArray[updateIdx] + 1;
-
-    const newPoll = await Poll.findByIdAndUpdate({ _id }, { $set: { "votes": votesArray } }, { new: true });
-    res.contentType("application/json");
-
-    res.json(JSON.stringify(newPoll));
 })
 
 module.exports = router;
