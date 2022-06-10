8b66d073d06ec909c0381eb1b87f63bd0cd7a517
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-10 13:03:01 +0530

    Finished making the skeleton of error page

---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,25 @@
+import { Button, Container, Typography } from "@mui/material";
+
+function ErrorPage(props) {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h1" component="div" gutterBottom textAlign="center">
+        404
+      </Typography>
+
+      <div className="error-page-background">
+        <img src="/images/dribbble_1.gif" alt="" />
+      </div>
+      <Typography variant="h2" component="p" gutterBottom textAlign="center">
+        Look like you're lost
+      </Typography>
+      <Typography variant="h4" component="p" gutterBottom textAlign="center">
+        the page you are looking for not avaible!
+      </Typography>
+      <Button variant="contained" color="secondary" size="large">
+        Go Home
+      </Button>
+    </Container>
+  );
+}
+export default ErrorPage;
