42177d71032d50ff206546512e8c2d9478a92fd5
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-11 20:05:03 +0530

    Added username and password felid in the user schema

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -3,6 +3,14 @@ const mongoose = require("mongoose");
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
     email: {
         type: String,
         required: true
