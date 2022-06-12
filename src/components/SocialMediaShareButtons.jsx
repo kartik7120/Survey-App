111ddd7c6fc53641e296ee5dcbb54d9e3d96e155
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-12 21:42:13 +0530

    Removing CardHeader component from imports

--- src/components/SocialMediaShareButtons.jsx
+++ src/components/SocialMediaShareButtons.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
 import {
   EmailShareButton,
   FacebookShareButton,
