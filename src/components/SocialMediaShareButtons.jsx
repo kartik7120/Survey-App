40133b102c493fa0032d5d3c52616a4571c53365
Kartik Shukla <kaartikshukla7120@gmail.com>
2022-06-13 08:24:12 +0530

    Added some scale up animation using framer motion

--- src/components/SocialMediaShareButtons.jsx
+++ src/components/SocialMediaShareButtons.jsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 import {
   EmailShareButton,
   FacebookShareButton,
@@ -26,47 +27,57 @@ function SocialMediaShareButtons(props) {
         Share
       </Typography>
       <CardContent style={{ display: "flex", justifyContent: "space-evenly" }}>
-        <RedditShareButton
-          url="http://localhost:3000/poll/6290601894c72cbd8787af53"
-          children={<div></div>}
-          openShareDialogOnClick
-          title={props.state.title}
-        >
-          <RedditIcon round={true}></RedditIcon>
-        </RedditShareButton>
-        <FacebookShareButton
-          url="http://localhost:3000/poll/6290601894c72cbd8787af53"
-          children={<div></div>}
-          openShareDialogOnClick
-          hashtag="Poll"
-        >
-          <FacebookIcon round={true}></FacebookIcon>
-        </FacebookShareButton>
-        <WhatsappShareButton
-          url="http://localhost:3000/poll/6290601894c72cbd8787af53"
-          children={<div></div>}
-          openShareDialogOnClick
-          title={props.state.title}
-        >
-          <WhatsappIcon round={true}></WhatsappIcon>
-        </WhatsappShareButton>
-        <TwitterShareButton
-          url="http://localhost:3000/poll/6290601894c72cbd8787af53"
-          children={<div></div>}
-          openShareDialogOnClick
-          title={props.state.title}
-        >
-          <TwitterIcon round={true}></TwitterIcon>
-        </TwitterShareButton>
-        <EmailShareButton
-          url="http://localhost:3000/poll/6290601894c72cbd8787af53"
-          children={<div></div>}
-          openShareDialogOnClick
-          subject={props.state.title}
-          body="http://localhost:3000/poll/6290601894c72cbd8787af53"
-        >
-          <EmailIcon round={true}></EmailIcon>
-        </EmailShareButton>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <RedditShareButton
+            url="http://localhost:3000/poll/6290601894c72cbd8787af53"
+            children={<div></div>}
+            openShareDialogOnClick
+            title={props.state.title}
+          >
+            <RedditIcon round={true}></RedditIcon>
+          </RedditShareButton>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <FacebookShareButton
+            url="http://localhost:3000/poll/6290601894c72cbd8787af53"
+            children={<div></div>}
+            openShareDialogOnClick
+            hashtag="Poll"
+          >
+            <FacebookIcon round={true}></FacebookIcon>
+          </FacebookShareButton>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <WhatsappShareButton
+            url="http://localhost:3000/poll/6290601894c72cbd8787af53"
+            children={<div></div>}
+            openShareDialogOnClick
+            title={props.state.title}
+          >
+            <WhatsappIcon round={true}></WhatsappIcon>
+          </WhatsappShareButton>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <TwitterShareButton
+            url="http://localhost:3000/poll/6290601894c72cbd8787af53"
+            children={<div></div>}
+            openShareDialogOnClick
+            title={props.state.title}
+          >
+            <TwitterIcon round={true}></TwitterIcon>
+          </TwitterShareButton>
+        </motion.div>
+        <motion.div whileHover={{ scale: 1.2 }}>
+          <EmailShareButton
+            url="http://localhost:3000/poll/6290601894c72cbd8787af53"
+            children={<div></div>}
+            openShareDialogOnClick
+            subject={props.state.title}
+            body="http://localhost:3000/poll/6290601894c72cbd8787af53"
+          >
+            <EmailIcon round={true}></EmailIcon>
+          </EmailShareButton>
+        </motion.div>
       </CardContent>
     </Card>
   );
